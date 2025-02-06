f4e230cc504c8d11b20d694d53aa1bce938e02e8
24LP8796 24LP8796 <amgaa0329@gmail.com>
2025-02-06 16:45:15 +0800

    firstcommit

---
+++ controllers/create-user.tsx
@@ -0,0 +1,20 @@
+import { Request, Response, Router } from "express";
+export const userRouter = Router();
+import { Prisma } from "@prisma/client";
+import { prisma } from "..";
+
+userRouter.get("/", async (req: Request, res: Response) => {});
+
+userRouter.post("/", async (req: Request, res: Response) => {
+  const { email, password, username } = req.body;
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        email: email,
+        password: password,
+        username: username,
+      },
+    });
+    res.json(newUser);
+  } catch {}
+});
--- index.ts
+++ index.ts
@@ -1,20 +1,33 @@
 import { PrismaClient } from "@prisma/client";
+export const prisma = new PrismaClient();
+import express, { Request, Response } from "express";
+import { userRouter } from "./controllers/create-user";
+const cors = require("cors");
+const PORT = 4000;
+const app = express();
 
-const prisma = new PrismaClient();
+app.use(cors());
+app.use(express.json());
 
-const addBankAccount = async () => {
-  const newAccount = await prisma.bankCard.create({
-    data: {
-      country: "Mongolia",
-      firstName: "Amgalanbaatar",
-      lastName: "Surenjav",
-      cardNumber: "1234 1234 1234 1234",
-      expiryDate: new Date("2020-07-10"),
-      userId: "1asdfkwe12",
-    },
-  });
-  console.log(newAccount);
-};
+app.use("/auth/sign-up", userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on http://localhost:${PORT}`);
+});
+
+// const addBankAccount = async () => {
+//   const newAccount = await prisma.bankCard.create({
+//     data: {
+//       country: "Mongolia",
+//       firstName: "Amgalanbaatar",
+//       lastName: "Surenjav",
+//       cardNumber: "1234 1234 1234 1234",
+//       expiryDate: new Date("2020-07-10"),
+//       userId: "1asdfkwe12",
+//     },
+//   });
+//   console.log(newAccount);
+// };
 // addBankAccount()
 //   .then(async () => {
 //     await prisma.$disconnect();
@@ -24,4 +37,3 @@ const addBankAccount = async () => {
 //     await prisma.$disconnect();
 //     process.exit(1);
 //   });
-addBankAccount();
