3efd38327d256bc2390323c7d7e3a75732b0549f
Enkhsolongo10 <rrainbow1018@gmail.com>
2025-02-14 20:29:39 +0800

    backend

--- controllers/bank-card/create-bankcard.ts
+++ controllers/bank-card/create-bankcard.ts
@@ -2,6 +2,16 @@ import { Request, Response, Router } from "express";
 
 import { prisma } from "../..";
 
+export const fetchBankCards = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const bankCards = await prisma.bankCard.findUnique({
+    where: {
+      userId: id,
+    },
+  });
+  res.json(bankCards);
+};
+
 export const createBankCard = async (req: Request, res: Response) => {
   const { id } = req.params;
   const { country, firstName, lastName, cardNumber, expiryDate } = req.body;
@@ -22,12 +32,28 @@ export const createBankCard = async (req: Request, res: Response) => {
     res.send(e);
   }
 };
-export const fetchBankCards = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  const bankCards = await prisma.bankCard.findUnique({
-    where: {
-      userId: id,
-    },
-  });
-  res.json(bankCards);
+
+export const updateBankCard = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const { country, firstName, lastName, cardNumber, cvc, expiryDate } = req.body;
+  try {
+    const newBankcard = await prisma.bankCard.update({
+      where:{
+        id,
+      },
+      data: {
+        userId: "709",
+        expiryDate: new Date(expiryDate),
+        country,
+        firstName,
+        lastName,
+        cardNumber,
+      },
+    });
+    res.json(newBankcard);
+  } catch (e) {
+    console.log(e);
+    res.send(e);
+  }
 };
+
