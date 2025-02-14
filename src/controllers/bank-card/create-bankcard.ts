00796a0ddee7df63f3181d68fc09d47f71f2ac88
Enkhsolongo10 <rrainbow1018@gmail.com>
2025-02-14 21:03:06 +0800

    1 bank card backend (#9)
    * merge
    
    * merge2
    
    * bank
    
    * backend
    
    ---------
    
    Co-authored-by: 24LP1557 24LP1557 <24LP1557@G305-CC0109.local>

--- src/controllers/bank-card/create-bankcard.ts
+++ src/controllers/bank-card/create-bankcard.ts
@@ -1,6 +1,16 @@
 import { Request, Response, Router } from "express";
 import { prisma } from "../..";
 
+export const fetchBankCards = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const bankCards = await prisma.bankCard.findUnique({
+    where: {
+      userId: id,
+    },
+  });
+  res.json(bankCards);
+};
+
 export const createBankCard = async (req: Request, res: Response) => {
   const { id } = req.params;
   const { country, firstName, lastName, cardNumber, expiryDate } = req.body;
