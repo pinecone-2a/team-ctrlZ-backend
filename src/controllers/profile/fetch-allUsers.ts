cc5880a13e1f4ec9683c9e95e25e326b449358bd
24LP8796 24LP8796 <amgaa0329@gmail.com>
2025-02-14 21:22:14 +0800

    23

--- src/controllers/profile/fetch-allUsers.ts
+++ src/controllers/profile/fetch-allUsers.ts
@@ -2,12 +2,9 @@ import { Request, Response } from "express";
 import { prisma } from "../..";
 
 export const fetchAllUsers = async (req: Request, res: Response) => {
+  console.log("hello");
   try {
-    const users = await prisma.user.findMany({
-      include: {
-        profile: true,
-      },
-    });
+    const users = await prisma.user.findMany();
     res.json(users);
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
