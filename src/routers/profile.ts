dd21081b8e816ac3d47cc4c766fb8c91736a5037
24LP8796 24LP8796 <amgaa0329@gmail.com>
2025-02-14 21:41:57 +0800

    13

--- src/routers/profile.ts
+++ src/routers/profile.ts
@@ -5,10 +5,10 @@ import { createProfile } from "../controllers/profile/create-profile";
 import { fetchCurrentUser } from "../controllers/profile/get-current-user";
 import { fetchUser } from "../controllers/profile/fetch-user";
 import { updateProfile } from "../controllers/profile/update-profile";
-import { fetchAllUsers } from "../controllers/profile/fetch-allUsers";
+import { fetchAllusers } from "../controllers/profile/fetch-allUsers";
 
+profileRouter.get("/explore", fetchAllusers);
 profileRouter.get("/:id", fetchCurrentUser);
 profileRouter.get("/view/:username", fetchUser);
-profileRouter.get("/explore", fetchAllUsers);
 profileRouter.post("/:id", createProfile);
 profileRouter.put("/:profileId", updateProfile);
