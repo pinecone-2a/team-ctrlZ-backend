a9d8208e41cf105432e785cc9308e40b1e02ede8
24LP8796 24LP8796 <amgaa0329@gmail.com>
2025-02-16 20:56:39 +0800

    3

--- src/routers/donation.ts
+++ src/routers/donation.ts
@@ -3,6 +3,8 @@ export const donationRouter = Router();
 
 import { createDonation } from "../controllers/donation/create-donation";
 import { fetchReceivedDonation } from "../controllers/donation/fetch-received-donation";
+import { fetchTotalEarnings } from "../controllers/donation/fetch-total-earning";
 
+donationRouter.get("/total-earnings/:userId", fetchTotalEarnings);
 donationRouter.get("/:id", fetchReceivedDonation);
 donationRouter.post("/create-donation", createDonation);
