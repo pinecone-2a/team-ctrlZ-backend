f597947bca86dbf66a46c73c9033d48d7241f270
24LP8796 24LP8796 <amgaa0329@gmail.com>
2025-02-17 17:05:49 +0800

    url change

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://team-ctrl-z-frontend.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
