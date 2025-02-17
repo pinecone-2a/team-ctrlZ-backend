d696d6f4fe8029fdcc5a740ae5f46f4912664643
Boldchingis <your-email@example.com>
2025-02-17 15:26:02 +0800

    1

--- routers/user.ts
+++ routers/user.ts
@@ -1,8 +1,8 @@
 import { Request, Response, Router } from "express";
 export const userRouter = Router();
 
-import { createUser, fetchUsers } from "../controllers/user/create-user";
-import { signinController } from "../controllers/user/sign-in-controller";
+import { createUser, fetchUsers } from "../src/controllers/user/create-user";
+import { signinController } from "../src/controllers/user/sign-in-controller";
 
 userRouter.post("/sign-in", signinController);
 userRouter.post("/sign-up", createUser);
