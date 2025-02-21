d2c8a240939da5e057869a148de889a3fe13787a
Kawabatajt <amgaa0329@gmail.com>
2025-02-21 09:20:39 +0800

    3

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,13 @@ dotenv.config();
 const PORT = 4000;
 const app = express();
 
+app.options('*', (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(200);
+})
+
 app.use(
   cors({
     origin: [
@@ -25,12 +32,6 @@ app.use(
 );
 app.use(express.json());
 
-app.options('*', (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.sendStatus(200);
-})
 
 app.use("/auth", userRouter);
 app.use("/bank-card", bankCardRouter);
